9e5e4847e00363daeb6c5e544fde9b1e6d61db4b
clistenes <clistenes808@gmail.com>
2019-08-22 23:11:57 -0300

    backend almost finished

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://tindev-omnistack:tindevomnistack@cluster0-tbgk9.mongodb.net/tindev?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
