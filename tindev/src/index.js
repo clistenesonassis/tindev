08cbf79e5c19bd1facf950faec75aa792fdb28f0
clistenes <clistenes.araujo@gmail.com>
2020-04-06 13:12:59 -0300

    starting mobile

---
+++ tindev/src/index.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  Login
+} from './pages/login';
+
+const App: () => React$Node = () => {
+  return (
+    <Login />
+  );
+};
+
+export default App;
